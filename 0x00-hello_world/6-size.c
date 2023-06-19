2302c099b971737f721a89f6ce351d89b12f6e2e
OMadonna <maddynice904@gmail.com>
2023-06-19 11:37:50 -0700

    outputing with printf

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
 
 /**
- * main - Prints the size of various types based onthe computer it is compiled and run on.
+ * main - Prints the size of various types based on
+ * the computer it is compiled and run on..
+ *
  * Return: Always 0.
  */
 int main(void)
@@ -11,6 +13,5 @@ int main(void)
 	printf("Size of a long int: %zu byte(s)\n", sizeof(long int));
 	printf("Size of a long long int: %zu byte(s)\n", sizeof(long long int));
 	printf("Size of a float: %zu byte(s)\n", sizeof(float));
-
 	return (0);
 }
